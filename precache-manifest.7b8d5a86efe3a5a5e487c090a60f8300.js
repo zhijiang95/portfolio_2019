37bd953c4ec0c3e6848e4b749770acb59cf3081c
zhijiang wang <wangzhijiang333@icloud.com>
2021-01-29 09:44:01 +1000

    initial

---
+++ precache-manifest.7b8d5a86efe3a5a5e487c090a60f8300.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09822f89b07f58f4c73aa93981a60dc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fd63ad5718c0426fb3f",
+    "url": "/static/css/main.98ce2909.chunk.css"
+  },
+  {
+    "revision": "5cdbe6375d2e8648d5c7",
+    "url": "/static/js/2.995211c4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.995211c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd63ad5718c0426fb3f",
+    "url": "/static/js/main.1fa7b11f.chunk.js"
+  },
+  {
+    "revision": "74ca039aabce4e4a9a3c",
+    "url": "/static/js/runtime-main.d7e80684.js"
+  },
+  {
+    "revision": "a4f3566ae43ca8bc71e340f27674d941",
+    "url": "/static/media/Close.a4f3566a.png"
+  },
+  {
+    "revision": "8d079a11732770a23b1dbcb3dc08cb8f",
+    "url": "/static/media/Facebook.8d079a11.png"
+  },
+  {
+    "revision": "e5d7b714db55e4877ebbd41c65f00937",
+    "url": "/static/media/Github.e5d7b714.png"
+  },
+  {
+    "revision": "694a036103d73cae2ae60964f227343d",
+    "url": "/static/media/Linkedin.694a0361.png"
+  },
+  {
+    "revision": "c27c8cdc493303f37c602055f5ab6f45",
+    "url": "/static/media/c1.c27c8cdc.png"
+  },
+  {
+    "revision": "d666ccc54f5e7baadb6d8cd6fd3b802f",
+    "url": "/static/media/c2.d666ccc5.png"
+  },
+  {
+    "revision": "df610d393870dc7a190a4e22e6904859",
+    "url": "/static/media/c3.df610d39.png"
+  },
+  {
+    "revision": "1c6311ab366821ef0830fef0e1150d89",
+    "url": "/static/media/c4.1c6311ab.png"
+  },
+  {
+    "revision": "3a6d3e9b6ab99b35c253152bb11ee56b",
+    "url": "/static/media/chronic.3a6d3e9b.png"
+  },
+  {
+    "revision": "857fd79dfd7bd025e4cbb2169cd46e03",
+    "url": "/static/media/hand.857fd79d.png"
+  },
+  {
+    "revision": "8d4d1d27a7f9411f083aae567efedd5a",
+    "url": "/static/media/home.8d4d1d27.png"
+  },
+  {
+    "revision": "5dc96e0a2646ab8ce9a5715954872ca2",
+    "url": "/static/media/login.5dc96e0a.png"
+  },
+  {
+    "revision": "8d4d1d27a7f9411f083aae567efedd5a",
+    "url": "/static/media/physicB.8d4d1d27.png"
+  },
+  {
+    "revision": "465a754889705e1545063c057a7baecd",
+    "url": "/static/media/pixcelboost.465a7548.png"
+  },
+  {
+    "revision": "43e8bef195d8bee8f1ca69fd1a117e27",
+    "url": "/static/media/react.43e8bef1.png"
+  },
+  {
+    "revision": "776c3dde015e7691eebe43acd7c79eb8",
+    "url": "/static/media/rest.776c3dde.png"
+  },
+  {
+    "revision": "1be80013c24ae08b7f093ef1df2566a2",
+    "url": "/static/media/results.1be80013.png"
+  },
+  {
+    "revision": "08735f93261720d0437c2bbd2adbea15",
+    "url": "/static/media/resume.08735f93.png"
+  },
+  {
+    "revision": "fcf40d6a94df7a5a340cc3d9e9f1aca1",
+    "url": "/static/media/sass.fcf40d6a.png"
+  },
+  {
+    "revision": "c22d66c8a8b16583bca4a79b9e282001",
+    "url": "/static/media/survey.c22d66c8.png"
+  },
+  {
+    "revision": "d17ed18d13c23838080b79f850f91eec",
+    "url": "/static/media/tree.d17ed18d.jpeg"
+  },
+  {
+    "revision": "af6c454ac82b2718fd81a6135ef6e17b",
+    "url": "/static/media/treeB.af6c454a.JPG"
+  }
+]);
